6836b3ebccc3fc7775a073b529ded41b7c97c7d0
Raina Moon <mds6425@gmail.com>
2025-08-24 03:13:54 +0900

    feat : 퍼블리싱 날짜 format 유틸 함수로 표현

--- src/app/pages/ArticleDetailPage.tsx
+++ src/app/pages/ArticleDetailPage.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { useArticleByIdQuery } from '../../entities/article/hooks'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useArticleByIdQuery } from "../../entities/article/hooks";
+import { useParams } from "react-router-dom";
+import { formatDate } from "../utils/date";
 
 const ArticleDetailPage = () => {
-    const {id} = useParams()
-    const {data} = useArticleByIdQuery(String(id))
+  const { id } = useParams();
+  const { data } = useArticleByIdQuery(String(id));
   return (
     <div>
-        <h1>{data?.title}</h1>
-        {data?.image && <img src={data.image} alt={data.title} style={{width: '100%', objectFit: 'cover'}} />}
-        <p>{data?.content}</p>
-        <a href={data?.url} target="_blank" rel="noopener noreferrer">Read full article</a>
+      <h1>{data?.title}</h1>
+      {data?.publishedAt && <p>{formatDate(data.publishedAt)}</p>}
+      {data?.image && (
+        <img
+          src={data.image}
+          alt={data.title}
+          style={{ width: "100%", objectFit: "cover" }}
+        />
+      )}
+      <p>{data?.content}</p>
+      <a href={data?.url} target="_blank" rel="noopener noreferrer">
+        Read full article
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default ArticleDetailPage
+export default ArticleDetailPage;
