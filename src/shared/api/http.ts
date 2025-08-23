0e280b0c0c15ba44fb6804855496df38377d0774
Raina Moon <mds6425@gmail.com>
2025-08-24 01:21:12 +0900

    feat : gnews api key 추가

--- src/shared/api/http.ts
+++ src/shared/api/http.ts
@@ -3,7 +3,9 @@ import axios from 'axios'
 export const http = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
-        'Content-Type': 'application/json'
+        Authorization: `Bearer ${import.meta.env.GNEWS_API_KEY}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
     },
     timeout: 10000
 })
