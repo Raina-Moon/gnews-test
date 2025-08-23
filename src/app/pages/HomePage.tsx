7a37182be2d599d765b4c67a76c14a0eb50d4b57
Raina Moon <mds6425@gmail.com>
2025-08-24 02:46:27 +0900

    refactor : 메인페이지에서 쓰는 newcard를 props 형태로 리팩토링

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import NewsCard from '../../entities/ui/NewsCard'
+import React from "react";
+import NewsCard from "../../entities/ui/NewsCard";
+import { useTopNewsQuery } from "../../entities/article/hooks";
 
 const HomePage = () => {
+  const { data } = useTopNewsQuery();
+
   return (
     <div>
-        <NewsCard />
+      <ul>
+        {data?.articles.map((article) => (
+          <NewsCard article={article} />
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/entities/ui/NewsCard.tsx
+++ src/entities/ui/NewsCard.tsx
@@ -1,34 +1,31 @@
 import React from "react";
-import { useTopNewsQuery } from "../article/hooks";
+import type { ArticleItem } from "../article/types";
 
-const NewsCard = () => {
-  const { data } = useTopNewsQuery();
-  
+interface NewsCardProps {
+  article: ArticleItem;
+}
+
+const NewsCard: React.FC<NewsCardProps> = ({ article }) => {
   return (
-    <div>
-      <ul>
-        {data?.articles.map((article) => (
-          <li key={article.url}>
-            <a href={article.url} target="_blank" rel="noopener noreferrer">
-              <h3>{article.title}</h3>
-              <p>{article.description}</p>
-              {article.image && (
-                <img
-                  src={article.image}
-                  alt={article.title}
-                  style={{
-                    width: "100px",
-                    height: "100px",
-                    objectFit: "cover",
-                  }}
-                />
-              )}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <li key={article.id}>
+      <a href={article.url} target="_blank" rel="noopener noreferrer">
+        <h3>{article.title}</h3>
+        <p>{article.description}</p>
+        {article.image && (
+          <img
+            src={article.image}
+            alt={article.title}
+            style={{
+              width: "100px",
+              height: "100px",
+              objectFit: "cover",
+            }}
+          />
+        )}
+      </a>
+    </li>
   );
 };
 
 export default NewsCard;
+
