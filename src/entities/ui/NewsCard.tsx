870afaf14c8c93773e4c2b23cf93a5d003d6003c
Raina Moon <mds6425@gmail.com>
2025-08-24 02:56:33 +0900

    style : 홈페이지, 뉴스카드 스타일 적용

--- src/entities/ui/NewsCard.tsx
+++ src/entities/ui/NewsCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { ArticleItem } from "../article/types";
+import styled from "styled-components";
 
 interface NewsCardProps {
   article: ArticleItem;
@@ -7,25 +8,65 @@ interface NewsCardProps {
 
 const NewsCard: React.FC<NewsCardProps> = ({ article }) => {
   return (
-    <li key={article.id}>
+    <ListWrapper key={article.id}>
       <a href={article.url} target="_blank" rel="noopener noreferrer">
         <h3>{article.title}</h3>
-        <p>{article.description}</p>
         {article.image && (
           <img
             src={article.image}
             alt={article.title}
             style={{
-              width: "100px",
-              height: "100px",
+              width: "100%",
               objectFit: "cover",
             }}
           />
         )}
+        <p>{article.description}</p>
       </a>
-    </li>
+    </ListWrapper>
   );
 };
 
 export default NewsCard;
 
+const ListWrapper = styled.li`
+  list-style: none;
+  margin-bottom: 20px;
+  padding: 15px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s ease, box-shadow 0.3s ease;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  
+  &:hover {
+    transform: translateY(-4px);
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+    border-color: #aaa;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    gap: 10px;
+  }
+
+  h3 {
+    margin: 0 0 10px 0;
+    font-size: 1.2em;
+  }
+
+  p {
+    margin: 0;
+    color: #555;
+    flex: 1;
+  }
+
+  img {
+    border-radius: 8px;
+  }
+`
