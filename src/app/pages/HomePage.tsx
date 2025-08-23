dd01c7ce2fdb587608b9b0d59548d73ce582e5b9
Raina Moon <mds6425@gmail.com>
2025-08-24 03:46:28 +0900

    feat : 검색바 홈페이지에 추가해서 article list에 패칭칭

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import NewsCard from "../../entities/ui/NewsCard";
 import { useTopNewsQuery } from "../../entities/article/hooks";
 import styled from "styled-components";
+import NewsSearchBar from "../../entities/ui/NewsSearchBar";
+import { useSearchArticles } from "../../entities/search/hooks";
 
 const HomePage = () => {
-  const { data } = useTopNewsQuery();
+  const [query, setQuery] = useState("");
+  const q = query.trim();
+  const { data: top } = useTopNewsQuery();
+  const { data: searchResults } = useSearchArticles(q);
+
+  const articles = q ? searchResults : top;
 
   return (
     <div>
+      <NewsSearchBar query={query} onSearch={setQuery} />
       <ListWrapper>
-        {data?.articles.map((article) => (
+        {articles?.articles.map((article) => (
           <NewsCard article={article} />
         ))}
       </ListWrapper>
+
+      {articles?.articles.length === 0 && <div>No articles found.</div>}
     </div>
   );
 };
@@ -20,9 +30,9 @@ const HomePage = () => {
 export default HomePage;
 
 const ListWrapper = styled.ul`
-padding: 20px;
-margin: 0 auto;
-display: grid;
-grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-gap: 20px;
-`
+  padding: 20px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 20px;
+`;
