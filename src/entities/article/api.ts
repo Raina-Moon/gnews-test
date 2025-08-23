5752aa415a5217fcd84e10bc0d1d5169e505d0ea
Raina Moon <mds6425@gmail.com>
2025-08-24 03:02:25 +0900

    feat : 뉴스 디테일 불러오는 api, 훅 생성

--- src/entities/article/api.ts
+++ src/entities/article/api.ts
@@ -12,3 +12,19 @@ export const fetchTopArticles = async () => {
   });
   return res.data;
 };
+
+export const fetchArticleById = async (id: string) => {
+  const res = await http.get<Articles>("/top-headlines", {
+    params: {
+      country: "us",
+      lang: "en",
+      max: 10,
+      apikey: import.meta.env.VITE_GNEWS_API_KEY,
+    },
+  });
+  const article = res.data.articles.find((article) => article.id === id);
+  if (!article) {
+    throw new Error("Article not found");
+  }
+  return article;
+};
--- src/entities/article/hooks.ts
+++ src/entities/article/hooks.ts
@@ -1,10 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchTopArticles } from "./api";
+import { fetchArticleById, fetchTopArticles } from "./api";
 import type { Articles } from "./types";
 
 export const useTopNewsQuery = () => {
   return useQuery<Articles>({
     queryKey: ["top-articles"],
     queryFn: () => fetchTopArticles(),
+  });
+};
+
+export const useArticleByIdQuery = (id: string) => {
+  return useQuery({
+    queryKey: ["article", id],
+    queryFn: () => fetchArticleById(id),
   });
 };
