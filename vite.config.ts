1e8ea58459999b34bbd9bc4e119301c823877270
Raina Moon <mds6425@gmail.com>
2025-08-24 02:03:18 +0900

    chore : CORS 오류로 인한 proxy 우회

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/gnews": {
+        target: "https://gnews.io",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/gnews/, ""),
+      },
+    },
+  },
+});
