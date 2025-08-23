f7127ead97f0c543d97688f2d521cc4344601f25
Raina Moon <mds6425@gmail.com>
2025-08-24 02:05:52 +0900

    feat : 뉴스 카드 추가

---
+++ src/app/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NewsCard from '../../entities/ui/NewsCard'
+
+const HomePage = () => {
+  return (
+    <div>
+        <NewsCard />
+    </div>
+  )
+}
+
+export default HomePage
