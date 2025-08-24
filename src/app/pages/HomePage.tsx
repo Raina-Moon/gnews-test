06bf770c52ef80fa3345898348a560090e977b6e
Raina Moon <mds6425@gmail.com>
2025-08-24 12:16:50 +0900

    style : 뉴스 카드 스켈레톤 생성 및 구현

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -4,21 +4,26 @@ import { useTopNewsQuery } from "../../entities/article/hooks";
 import styled from "styled-components";
 import NewsSearchBar from "../../entities/ui/NewsSearchBar";
 import { useDebounce, useSearchArticles } from "../../entities/search/hooks";
+import SkeletonNewsCard from "../../entities/ui/SkeletonNewsCard";
 
 const HomePage = () => {
   const [query, setQuery] = useState("");
   const debounced = useDebounce(query, 300);
   const q = debounced.trim();
-  const { data: top } = useTopNewsQuery();
+  const { data: top, isLoading: topLoading } = useTopNewsQuery();
   const {
     data: searchResults,
     isLoading: searchLoading,
     isFetching: searchFetching,
   } = useSearchArticles(q);
 
-  const articles = q ? searchResults : top;
+  const isSearching = q.length > 0;
 
-  if (q && (searchLoading || searchFetching)) return <div>Searching…</div>;
+  const showSkeletons =
+    (!isSearching && topLoading) ||
+    (isSearching && (searchLoading || searchFetching));
+
+  const articles = q ? searchResults : top;
 
   return (
     <div>
@@ -26,11 +31,21 @@ const HomePage = () => {
         <NewsSearchBar query={query} onSearch={setQuery} />
         {searchResults && <p>Results: {searchResults?.articles.length}</p>}
       </SearchBarWrapper>
+      {showSkeletons ? (
+        <ListWrapper>
+        {Array.from({ length: 6 }).map((_, index) => (
+          <li key={index}>
+          <SkeletonNewsCard />
+          </li>
+        ))}
+      </ListWrapper>
+      ) : (
       <ListWrapper>
         {articles?.articles.map((article) => (
           <NewsCard article={article} />
         ))}
       </ListWrapper>
+      )}
 
       {articles?.articles.length === 0 && <div>No articles found.</div>}
     </div>
@@ -45,17 +60,21 @@ const ListWrapper = styled.ul`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   gap: 20px;
+
+  li { 
+    list-style: none;
+  }
 `;
 
 const SearchBarWrapper = styled.div`
-margin: 20px auto;
+  margin: 20px auto;
   max-width: 1000px;
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 10px;
 
-  p{
+  p {
     margin: 0;
     font-size: 14px;
     color: #555;
